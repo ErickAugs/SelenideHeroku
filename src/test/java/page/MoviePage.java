3f026c71ee3d7c37b8eb204e21caab1995096df2
erickAugusto <erickaugs@gmail.com>
2022-05-09 23:57:39 -0300

    Web Elements e array

--- src/test/java/page/MoviePage.java
+++ src/test/java/page/MoviePage.java
@@ -1,6 +1,14 @@
 package page;
 
-import static com.codeborne.selenide.Selenide.$;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import models.MovieModel;
+import org.openqa.selenium.Keys;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 
 public class MoviePage {
 
@@ -9,8 +17,27 @@ public class MoviePage {
         return this;
     }
 
-    public MoviePage title(String filme){
-        $("input[name=title]").sendKeys(filme);
+    public MoviePage title(MovieModel movieModel){
+        $("input[name=title]").sendKeys(movieModel.title);
+        this.selectStatus(movieModel.status);
+        $("input[name=year]").setValue(movieModel.year.toString());
+        $("input[name=release_date]").setValue(movieModel.releaseDate);
+        this.imputCast(movieModel.cast);
+        $("textarea[name=overview]").setValue(movieModel.plot);
+
         return this;
+    }
+
+    private void imputCast(List<String> cast){
+        SelenideElement element = $(".cast");
+        for (String actor : cast){
+            element.setValue(actor);
+            element.sendKeys(Keys.TAB);
+        }
+    }
+
+    private void selectStatus(String status){
+        $("input[placeholder=Status").click();
+        $$("ul li span").findBy(text(status)).click();
     }
 }
--- src/test/java/tests/MovieTests.java
+++ src/test/java/tests/MovieTests.java
@@ -1,8 +1,12 @@
 package tests;
 
 import hooks.GeneralHooks;
+import models.MovieModel;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static com.codeborne.selenide.Condition.text;
 
@@ -20,6 +24,20 @@ public class MovieTests extends GeneralHooks {
     @Test
     public void registrarNovoFilme(){
         String title = "Senhor dos Aneis";
-        movie.adcFilme().title(title);
+        String status = "Pré-venda";
+        String ano = "2022";
+        String dtLanc = "16/05/2022";
+        List<String> cast = Arrays.asList("Orlando Blum", "Kevin Hart", "Dannyy Glover");
+        String sinopse = "A sociedade do ano ressurge para lutar contra mordor";
+
+       MovieModel movieData = new MovieModel(
+               "Senhor dos Aneis",
+               "Pré-venda",
+               2022,
+               "16/01/2022",
+               Arrays.asList("Orlando Blum", "Kevin Hart", "Dannyy Glover"),
+               "A sociedade do ano ressurge para lutar contra mordor");
+
+        movie.adcFilme().title(movieData);
     }
 }
